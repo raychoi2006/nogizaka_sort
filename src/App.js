0e94699569766d3312522f880dd97a08a6385af5
raychoi2021 <raychoi2021@gmail.com>
2021-07-21 19:07:50 +0800

    bubble sort

--- src/App.js
+++ src/App.js
@@ -7,167 +7,156 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        size:0,
-        items:[{item:0,equals:[]}],
-        current:{item:1,try:0,min:0,max:1},
-        c:0,
-        q:{},
-        printresult:false,
-        loading:true
+       memberList:[],
+       x:0,
+       y:1,
+       leftmember:{},
+       rightmember:{},
+       loading:true,
+       printresult:false,
+       sortedList:[],
+       totals: 0,
+       count:0
     };
-    this.getNextQuestion = this.getNextQuestion.bind(this);
   }
 
   componentDidMount() {
-      this.initialize()
-      console.log(member)
+    this.initialize()
   }
 
   initialize() {
-      //get member array
-       // var fruit = ["orange", "apple", "pear", "banana", "kiwifruit", "grapefruit", "peach", "cherry", "starfruit", "strawberry"];
-// var t = new PrefList(10), c = 0, q;
-// while (q = t.getQuestion()) {
+    var x = member.map((item,key)=>{
+      return {name:item.name,link:item.link,score:0}
+    })
+    var totalQ = this.findNumOfQuestion(x.length)
+    console.log(totalQ)
     this.setState({
-      size: member.length
-  })
-    var newq = this.getQuestion()
-    console.log(newq)
-    this.setState({
-        member: member,
-        q: newq,
-        loading: false
+      memberList: x,
+      leftmember: x[this.state.x],
+      rightmember: x[this.state.y],
+      totals: totalQ,
+      loading:false
     })
   }
 
-  getNextQuestion(answer) {
-    //var answer = preference(fruit[q.a], fruit[q.b]);
-    //     document.write("&nbsp;&rarr; " + [fruit[q.a], "no preference", fruit[q.b]][answer + 1] + "<BR>");
-    //     t.addAnswer(q.a, q.b, answer);
-    //     document.write(++c + ". " + fruit[q.a] + " or " + fruit[q.b] + "?<BR>");
-    if (this.state.q!=null) {
-        this.addAnswer(this.state.q.a,this.state.q.b,answer)
+  findNumOfQuestion(z) {
+    const newz = z-1
+    if(newz > 0)
+      return newz + this.findNumOfQuestion(newz)
+    else
+      return 0
+  }
+
+  getNextQuestion(choice) {
+    if(choice == 'one') {
+     // $scope.foodArray[x].score++;
+      var updatemList = [...this.state.memberList];
+      updatemList[this.state.x].score = updatemList[this.state.x].score+1;
+      this.setState({
+        memberList: updatemList
+      })
+    } else if(choice == 'two') {
+       // $scope.foodArray[y].score++; 
+       var updatemList = [...this.state.memberList];
+      updatemList[this.state.y].score = updatemList[this.state.y].score+1;
+      this.setState({
+        memberList: updatemList
+      })               
+    }
+    if(this.state.y < this.state.memberList.length -1) {
+        //y++;
+        //$scope.foodTwo = $scope.foodArray[y];
+        var nexty = this.state.y +1;
+        var nextcount = this.state.count +1;
         this.setState({
-            q: this.getQuestion()
+          count: nextcount,
+          y: nexty,
+          rightmember: this.state.memberList[nexty]
+        })
+    } else if(this.state.x < this.state.memberList.length -2) {
+        //x++;
+        //y = 1 + x;
+        //$scope.foodOne = $scope.foodArray[x];
+        //$scope.foodTwo = $scope.foodArray[y];
+        var nextx = this.state.x +1;
+        var nexty = nextx +1;
+        var nextcount = this.state.count +1;
+        this.setState({
+          count: nextcount,
+          x: nextx,
+          y: nexty,
+          leftmember: this.state.memberList[nextx],
+          rightmember: this.state.memberList[nexty]
         })
     } else {
-        this.sortPrint()
+        this.sortResult();   
     }
   }
 
-  sortPrint() {
-      // // PERFORM SORT BASED ON TABLE AND DISPLAY RESULT
-// var index = t.getOrder();
-// document.write("LIST IN ORDER:<BR>");
-// for (var i = 0, pos = 1; i < index.length; i++) {
-//     var pre = pos + ". ";
-//     for (var j = 0; j < index[i].length; j++) {
-//         document.write(pre + fruit[index[i][j]] + "<BR>");
-//         pre = "&nbsp;&nbsp;&nbsp;&nbsp;";
-//     }
-//     pos += index[i].length;
-// }
-    var index = this.getOrder()
-    console.log(index)
-  }
-
-    //this.size = n;
-    //this.items = [{item: 0, equals: []}];
-    //this.current = {item: 1, try: 0, min: 0, max: 1};
-  
-
-  addAnswer(x, y, pref) {
-  if (pref == 0) {
-      //this.items[this.current.try].equals.push(this.current.item);
-      //this.current = {item: ++this.current.item, try: 0, min: 0, max: this.items.length};
-      var joined = this.state.items[this.state.current.try].equals.concat(this.state.current.item)
-      this.setState({
-          items: joined,
-          current: {item: this.state.current.item+1, try: 0, min: 0, max: this.state.items.length}
-      })
-  } else {
-      if (pref == -1) 
-        //this.current.max = this.current.try
-        this.setState(prevState=>({
-           current:{
-               ...prevState.current,
-               max: this.state.current.try
-           } 
-        }))
-      else 
-        //this.current.min = this.current.try + 1;
-        this.setState(prevState=>({
-            current:{
-                ...prevState.current,
-                min: this.state.current.try+1
-            } 
-         }))
-      if (this.state.current.min == this.state.current.max) {
-         // this.items.splice(this.current.min, 0, {item: this.current.item, equals: []});
-          //this.current = {item: ++this.current.item, try: 0, min: 0, max: this.items.length};
-        var spliced = this.state.items.splice(this.state.current.min, 0, {item: this.state.item, equals: []})
-        this.setState({
-            items: spliced,
-            current: {item: this.state.current.item+1, try: 0, min: 0, max: this.state.items.length}
-        })
-        }
-    }
-  }
-
-  getQuestion() {
-    if (this.state.current.item >= member.length) return null;
-    //this.current.try = Math.floor((this.current.min + this.current.max) / 2);
-    var newtry = Math.floor((this.state.current.min + this.state.current.max) / 2)
-    this.setState(prevState=>({
-        current:{
-            ...prevState.current,
-            try: newtry
-        } 
-     }))
-    console.log(this.state.items)
-    console.log(this.state.current)
-    return({a: this.state.current.item, b: this.state.items[newtry].item});
-  }
-
-  getOrder() {
-    var index = [];
-    for (var i in this.state.items) {
-        var equal = [this.state.items[i].item];
-        for (var j in this.state.items[i].equals) {
-            equal.push(this.state.items[i].equals[j]);
-        }
-        index.push(equal);
-    }
-    return(index);
-  }
-
+  sortResult() {
+    //$scope.foodArray.sort(function(a,b) {
+      //return b.score - a.score;  
+    //});
+    //$scope.finished = true;
+    var sortedList = this.state.memberList.sort(function(a,b) {
+      return b.score - a.score;  
+    })
+    console.log(sortedList)
+    this.setState({
+      sortedList: sortedList,
+      printresult:true
+    })
+  }  
+ 
   render() {
 
   
   return (
     <div className="App">
-    {this.state.loading?(false):this.state.printresult?(false):
+    {this.state.loading?(false):this.state.printresult?
       <table className="centertable">
         <tbody>
           <tr>
+            <td colSpan="2">
+              Sorted Result
+            </td>
+          </tr>
+          {this.state.sortedList.map((item,key)=>{
+            return(
+              <tr>
+                <td>{key+1}.</td>
+                <td>{item.name}</td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+    :
+      <table className="centertable">
+        <tbody>
+          <tr>
+            <td>Progress:</td>
+            <td>{Math.floor((this.state.counts/this.state.totals))}%</td>
+          </tr>
+          <tr>
               <td>
-                <img src={this.state.member[this.state.q.a].link} onClick={()=>this.getNextQuestion(-1)}/>
+                <img src={this.state.leftmember.link} onClick={()=>this.getNextQuestion('one')}/>
               </td>
               <td>
-                <img src={this.state.member[this.state.q.b].link} onClick={()=>this.getNextQuestion(1)}/>
+                <img src={this.state.rightmember.link} onClick={()=>this.getNextQuestion('two')}/>
               </td>
           </tr>
           <tr>
             <td>
-              <p>{this.state.member[this.state.q.a].name}</p>
+              <p>{this.state.leftmember.name}</p>
             </td>
             <td>
-              <p>{this.state.member[this.state.q.b].name}</p>
+              <p>{this.state.rightmember.name}</p>
             </td>
           </tr>
           <tr>
             <td colSpan="2">
-              <button onClick={()=>this.getNextQuestion(0)}>No preference</button>
+              <button onClick={()=>this.getNextQuestion('nopref')}>No preference</button>
               </td>
           </tr>
           </tbody>
